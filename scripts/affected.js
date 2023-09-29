5fc62ad4642ee618babeb4785dc991491a69c3f1
anandsundramurthy <anand.sundramurthy@zattoo.com>
2023-09-29 15:29:49 +0200

    update

--- scripts/affected.js
+++ scripts/affected.js
@@ -5,7 +5,7 @@ const PROJECT_PATH = 'tests-examples/';
 
 
 (() => {
-    const files = execSync(`git diff main --name-only ${PROJECT_PATH}`).toString().split('\n').slice(0, -1);
+    const files = execSync(`git diff main --name-only -- ${PROJECT_PATH}`).toString().split('\n').slice(0, -1);
     // const component = files.split('tests-examples');
 
     const components = files.map((path) => { return path.split(`${PROJECT_PATH}`)[1].split('/')[0].concat('/') }).join(' ');
