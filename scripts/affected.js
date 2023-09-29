463ea58483b0593325f86716c9ec70f8523c4c0c
anandsundramurthy <anand.sundramurthy@zattoo.com>
2023-09-29 15:28:47 +0200

    Update

--- scripts/affected.js
+++ scripts/affected.js
@@ -1,14 +1,14 @@
 const { execSync } = require('node:child_process');
 
 
-const PROJECT_PATH = 'tests-examples';
+const PROJECT_PATH = 'tests-examples/';
 
 
 (() => {
     const files = execSync(`git diff main --name-only ${PROJECT_PATH}`).toString().split('\n').slice(0, -1);
     // const component = files.split('tests-examples');
 
-    const components = files.map((path) => { return path.split(`${PROJECT_PATH}/`)[1].split('/')[0].concat('/') }).join(' ');
+    const components = files.map((path) => { return path.split(`${PROJECT_PATH}`)[1].split('/')[0].concat('/') }).join(' ');
     console.log(components);
     execSync(`echo ::set-output name=files::${components}`);
 })();
