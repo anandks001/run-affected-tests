211776a30c7afaab84a0f4bd1c9200e08f34fee0
Anand S <anand.sundramurthy@zattoo.com>
2023-10-01 16:58:14 +0200

    Update

--- scripts/affected.js
+++ scripts/affected.js
@@ -13,7 +13,7 @@ const components = (files) => {
 
 
 const files = () => {
-    return execSync('git fetch origin main && git diff origin/main... --name-only -- tests-examples/ --decorate=short').toString().split('\n');
+    return execSync('git fetch origin main && git diff origin/main.. --name-only -- tests-examples/ --decorate=short').toString().split('\n');
 }
 
 (() => {
