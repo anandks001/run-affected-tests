37540db07475490d2ce88c91be1d2fc9376b19d4
Anand S <anand.sundramurthy@zattoo.com>
2023-10-04 11:07:45 +0200

    Update

--- tests-examples/test1/demo-todo-app.spec.js
+++ tests-examples/test1/demo-todo-app.spec.js
@@ -18,7 +18,7 @@ test.describe('New Todo', () => {
 
     // Create 1st todo.
     await newTodo.fill(TODO_ITEMS[0]);
-    await newTodo.press('Enter');
+    await newTodo.press('Ent er');
 
     // Make sure the list only has one todo item.
     await expect(page.getByTestId('todo-title')).toHaveText([
