a8b95c6fd96c5d56a9ca4007eeb1ecafe6d5f63b
Anand S <anand.sundramurthy@zattoo.com>
2023-10-04 10:38:28 +0200

    Update

--- scripts/affected.js
+++ scripts/affected.js
@@ -1,22 +1,22 @@
 const { execSync } = require('node:child_process');
 
 
-const PROJECT_PATH = 'tests-examples';
+// const PROJECT_PATH = 'tests-examples';
 
-const components = (files) => {
-    const temp = files.map((path) => { return path.split(`${PROJECT_PATH}`)[1].split('/')[0].concat('/') }).join(' ');
-    console.log(temp);
+// const components = (files) => {
+//     const temp = files.map((path) => { return path.split(`${PROJECT_PATH}`)[1].split('/')[0].concat('/') }).join(' ');
+//     console.log(temp);
 
-    return temp;
-    // execSync('git fetch origin main' && 'echo "git diff origin/main..HEAD --name-only -- tests-examples/" >> diff_files')
-}
+//     return temp;
+//     // execSync('git fetch origin main' && 'echo "git diff origin/main..HEAD --name-only -- tests-examples/" >> diff_files')
+// }
 
 
-const files = () => {
-    return execSync('git fetch origin affected && git diff origin/affected... --name-only -- tests-examples/').toString().split('\n');
-}
+// const files = () => {
+//     return execSync('git fetch origin affected && git diff origin/affected... --name-only -- tests-examples/').toString().split('\n');
+// }
 
 (() => {
-    const aff = files();
-   exec(`AFFECTED=${aff} >> $GITHUB_ENV`)
+    const aff = execSync('git fetch origin +refs/heads/main:refs/remotes/origin/main && git diff --merge-base origin/main HEAD');
+    execSync(`AFFECTED=${aff} >> $GITHUB_ENV`)
 })();
