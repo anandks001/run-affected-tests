84e10fe6f0910c22ef9d7638c7af3a80878792eb
Anand S <anand.sundramurthy@zattoo.com>
2023-10-04 15:01:43 +0200

    Update

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -2,4 +2,4 @@
 
 git fetch origin main
 AFFECTED=$(git diff origin/main..HEAD --name-only -- tests-examples/)
-echo "AFFECTEDFILES=$AFFECTED" >> "$GITHUB_ENV"
+echo "AFFECTED_FILES=${AFFECTED}" >> $GITHUB_ENV
