1f2357da36eb000dedbdece6b09fbedcda951bc9
Anand S <anand.sundramurthy@zattoo.com>
2023-10-04 15:30:56 +0200

    Update

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -5,7 +5,7 @@ AFFECTED=$(git diff origin/main..HEAD --name-only -- tests-examples/)
 arr=(`echo ${AFFECTED}`)
 for s in "${arr[@]}"; do
     echo "$s"
+    echo ${$s#*/}
+    echo ${$s%/*}
 done
 echo "AFFECTED_FILES=${arr}" >> $GITHUB_ENV
-echo "After setting env"
-echo "${AFFECTED_FILES}"
