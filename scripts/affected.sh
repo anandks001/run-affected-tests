e9cb2dd433dc2afc77f0ee8b05e56fe3f08fd00d
Anand S <anand.sundramurthy@zattoo.com>
2023-10-04 11:42:13 +0200

    Update

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -1,1 +1,3 @@
-git fetch origin main &&  git diff origin/main..HEAD --name-only -- tests-examples/ > mydiff && echo "mydiff = " && cat mydiff'
+#!/bin/bash
+
+git fetch origin main &&  git diff origin/main..HEAD --name-only -- tests-examples/ > mydiff && echo "mydiff = " && cat mydiff
