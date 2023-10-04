b6823d6d45beed56527af1d0b33c62e181a3e028
Anand S <anand.sundramurthy@zattoo.com>
2023-10-04 14:28:08 +0200

    Update

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git fetch origin main &&  AFFECTED=git diff origin/main..HEAD --name-only -- tests-examples/ >> $GITHUB_ENV && echo AFFECTED
+git fetch origin main && git diff origin/main..HEAD --name-only -- tests-examples/ > affected && echo affected
