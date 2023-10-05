293fc9565835afbcd99971b8571799adc9386008
Anand S <anand.sundramurthy@zattoo.com>
2023-10-05 13:51:53 +0200

    Update

--- scripts/affected.sh
+++ scripts/affected.sh
@@ -3,9 +3,11 @@
 git fetch origin main
 AFFECTED=$(git diff origin/main..HEAD --name-only -- tests-examples/)
 arr=(`echo ${AFFECTED}`)
-for s in "${arr[@]}"; do
-    echo "$s"
-    echo ${$s#*/}
-    echo ${$s%/*}
+for s in "${!arr[@]}"; do
+    component=${arr[$s]#tests-examples/}
+    arr[$s]=${component%/*}/
 done
-echo "AFFECTED_FILES=${arr}" >> $GITHUB_ENV
+
+FILES=${arr[@]}
+echo "$FILES"
+echo "AFFECTED_FILES=${FILES}" >> $GITHUB_ENV
