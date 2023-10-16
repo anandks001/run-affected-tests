f23beabf87544b971ec4e5bc869738c129923cdc
Anand S <anand.sundramurthy@zattoo.com>
2023-10-16 11:56:38 +0200

    update

--- scripts/aff.js
+++ scripts/aff.js
@@ -1,6 +1,6 @@
 const exec = require('node:child_process');
 
-import { PROJECT_EXAMPLE } from './env'
+import PROJECT_EXAMPLE from './env'
 
 (() => {
     exec(`echo "AFFECTED_PROJECT=${PROJECT_EXAMPLE}" >> $GITHUB_ENV`)
