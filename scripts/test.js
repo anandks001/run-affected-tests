e05fe5150487be646b56c2c4d72c4135f44c54b7
Anand S <anand.sundramurthy@zattoo.com>
2023-11-29 10:24:54 +0100

    Update

--- scripts/test.js
+++ scripts/test.js
@@ -1,14 +1,13 @@
 (async () => {
     let files = [];
-    files = process.argv[1];
-
+    files = process.argv[2]
     if (files.length === 0) {
         throw new Error('No affected files found');
     }
 
-    files.forEach((item) => {
+    files.forEach(item => {
         console.log("Files from script:", item);
-    })
+    });
     // const deepList = (await Promise.all(Object.values(Project).map((project) => {
     //     return getTenantItems(project, framework);
     // }))).flat().filter(Boolean);
